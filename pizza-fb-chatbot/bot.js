d4a165e25b65c132ded2fb131ceba98db599151c
aviv <aviv@innovid.com>
2018-04-22 17:54:28 +0300

    init

---
+++ pizza-fb-chatbot/bot.js
@@ -0,0 +1,45 @@
+const pizzas = require("./data/pizzas.json");
+
+const botBuilder = require("claudia-bot-builder");
+const fbTemplate = botBuilder.fbTemplate;
+
+const api = botBuilder(
+  message => {
+    if (message.postback) {
+      const [action, pizzaId] = message.text.split("|");
+      if (action === "DETAILS") {
+        const pizza = pizzas.find(pizza => pizza.id == pizzaId);
+        return [
+          `${pizza.name} has following ingredients: ` +
+            pizza.ingredients.join(", "),
+          new fbTemplate.Button("What else can I do for you?")
+            .addButton("Order", `ORDER|${pizzaId}`)
+            .addButton("Show all pizzas", "ALL_PIZZAS")
+            .get()
+        ];
+      } else if (action === "ORDER") {
+        const pizza = pizzas.find(pizza => pizza.id == pizzaId);
+        return `Thanks for ordering ${
+          pizza.name
+        }! I will let you know as soon as your pizza is ready.`;
+      }
+    }
+
+    const reply = new fbTemplate.Generic();
+
+    pizzas.forEach(pizza => {
+      reply
+        .addBubble(pizza.name)
+        .addImage(pizza.image)
+        .addButton("Details", `DETAILS|${pizza.id}`)
+        .addButton("Order", `ORDER|${pizza.id}`);
+    });
+
+    return [`Hello, here's our pizza menu:`, reply.get()];
+  },
+  {
+    platforms: ["facebook"]
+  }
+);
+
+module.exports = api;
